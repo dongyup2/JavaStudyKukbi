dd134465bc6385b95cc020d357473036e91258b9
dlehdduq22 <dlehdduq22@naver.com>
2023-05-22 20:01:20 +0900

    챕터 9 반복 다른 언어에서도 공통되는 반복문, for문과 while문등 배열에도 사용되므로 매우 중요한 문법이다.

---
+++ src/j09_반복/ContinueEx.java
@@ -0,0 +1,13 @@
+package j09_반복;
+
+public class ContinueEx {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+			continue;
+		}
+
+	}
+
+}
---
+++ src/j09_반복/Star1.java
@@ -0,0 +1,17 @@
+package j09_반복;
+
+public class Star1 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
