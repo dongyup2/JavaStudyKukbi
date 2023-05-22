a164f95b7db0e7112943fe1eac8bba7e8c3b2343
dlehdduq22 <dlehdduq22@naver.com>
2023-05-22 19:55:43 +0900

    챕터2 변수

---
+++ src/j02_변수/VaribleEx2.java
@@ -0,0 +1,27 @@
+package j02_변수;
+
+public class VaribleEx2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 문제1.
+		 * 이름: 이동엽
+		 * 
+		 * 문자형 변수 
+		 * firstName1
+		 * firstName2
+		 * lastName
+		 */
+		char firstName1 = '동';
+		char firstName2 = '엽';
+		char lastName = '이';
+		
+		System.out.println("이름: " + lastName + firstName1 + firstName2);
+		System.out.println(lastName);
+		System.out.println("" + (lastName + firstName1 + firstName2));
+		
+		char alphabet_a = 'a';
+		System.out.println(alphabet_a + 1);
+	}
+
+}
